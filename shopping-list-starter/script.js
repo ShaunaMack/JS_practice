c5925478ddabe7302a1d2cf1931c688270adfcd0
Shauna Mack <shaunakatemack@gmail.com>
2024-11-12 10:42:03 +1000

    add static items to the shopping list

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,7 +1,15 @@
-const addItem = () => {
+let shoppingListItems = ["milk", "eggs", "bread"];
+// locate the ul
+let listElement = document.getElementById("shopping-list-items");
 
-};
+// loop over each item in the array and plop a new li of the new item at the bottom of the ul
+for (const shoppingItem of shoppingListItems) {
+  console.log(shoppingItem);
+  let itemElement = document.createElement("li");
+  itemElement.innerText = shoppingItem;
+  listElement.appendChild(itemElement);
+}
 
-const updateItems = () => {
+const addItem = () => {};
 
-};
+const updateItems = () => {};
